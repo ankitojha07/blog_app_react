933f522f95b650f3f5c13ff27cd87ee1efb58b76
Ankit Ojha <ankitojha1409@gmail.com>
2022-11-21 00:20:37 +0530

    added tailwing css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl">Hi, Welcome to my blog page!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores culpa
+        alias facere vitae nostrum nam dolore, ad, quos sequi maiores molestias
+        non nemo labore odio et tenetur dignissimos provident voluptatum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil commodi
+        obcaecati perspiciatis soluta temporibus magnam provident accusantium
+        cupiditate recusandae laboriosam ut ex optio reiciendis animi aperiam
+        rerum nisi quibusdam autem iure nesciunt voluptate ea, enim iste dolor?
+        Laboriosam commodi nam inventore, cumque qui et alias? Molestias odit
+        numquam quis veritatis magni minus magnam a quidem aspernatur asperiores
+        atque placeat, ipsam laborum. Earum eveniet placeat id sit nemo
+        aspernatur incidunt rem. Eum provident placeat accusamus aspernatur
+        repellat laboriosam. Numquam aspernatur, quam unde ducimus eligendi
+        harum alias odit quaerat adipisci nisi amet error quidem saepe illum, ad
+        dolorum doloremque nobis iste! Similique!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
