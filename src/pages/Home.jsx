b7d7af9f126703e8ba62ed114368d0bfadfbc75e
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-14 15:46:41 +0530

    mapped json data to the component using map func

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,23 @@
 import React from "react";
-
-import { blog_food } from "../data/content_oprions";
+import articles from "./ArticleContent";
 
 const Home = () => {
   return (
-    <>
-      <div className="border-2 p-6 rounded-tr-lg rounded-l-xl">
-        <p className="border-2 border-emerald-600 font-medium bg-emerald-500 rounded-full w-20 text-center flex justify-center ">
-          {" "}
-          #diet
-        </p>
-        <h1 className="text-3xl font-bold mt-5">{blog_food.title}</h1>
-        <p className="mt-5">
-          {blog_food.description.substring(0, 200)}...{" "}
-          <a className="text-blue-500" href="#">
-            Read more
-          </a>
-        </p>
-      </div>
-    </>
+    <div>
+      {articles.map((data) => {
+        return (
+          <>
+            <div className="border-2 p-6 rounded-tr-lg rounded-l-xl">
+              <p className="border-2 border-emerald-600 font-medium bg-emerald-500 rounded-full  text-center flex justify-center w-1/2">
+                {"#" + data.name}
+              </p>
+              <h1 className="text-3xl font-bold mt-5">{data.title}</h1>
+              <p className="mt-5">{data.content}</p>
+            </div>
+          </>
+        );
+      })}
+    </div>
   );
 };
 
