5a0c4e4a21388b8069c66963793e9769b5a2a6c4
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-14 15:29:02 +0530

    passing data through objects

---
+++ src/data/content_oprions.js
@@ -0,0 +1,6 @@
+export const blog_food = {
+    title: "Healthy diet for students.",
+  description: 
+  "As a college student, you need to eat to fuel your body for a hectic season in your life, where you're busy with classes, studying, and work (and some play too). But you may feel like you don`t have the time, energy, or even the nutrition know-how you need. And maybe you are worried about that 'freshman 15' you've heard about."
+
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { articles } from "./ArticleContent";
+
+import { blog_food } from "../data/content_oprions";
 
 const Home = () => {
   return (
     <>
-      <div>
-        {articles.map((data, key) => {
-          return (
-            <div key={key}>
-              <h1>{data}</h1>
-            </div>
-          );
-        })}
+      <div className="border-2 p-6 rounded-tr-lg rounded-l-xl">
+        <p className="border-2 border-emerald-600 font-medium bg-emerald-500 rounded-full w-20 text-center flex justify-center ">
+          {" "}
+          #diet
+        </p>
+        <h1 className="text-3xl font-bold mt-5">{blog_food.title}</h1>
+        <p className="mt-5">
+          {blog_food.description.substring(0, 200)}...{" "}
+          <a className="text-blue-500" href="#">
+            Read more
+          </a>
+        </p>
       </div>
     </>
   );
