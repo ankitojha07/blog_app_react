3188546d6f6ebef1c8e2fca8bf05dd820a91ede8
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-14 16:05:07 +0530

    button and author details added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="border-b-4 border-blue-900 text-center fixed top-0 bg-blue-800 font-bold w-full text-lg text-white">
+    <nav className="border-b-4 border-cyan-500 text-center fixed top-0 bg-cyan-600 font-bold w-full text-lg text-white">
       <ul>
         <li className="inline-block py-4">
           <Link to="/" className="pl-6 pr-8 ">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,18 @@ const Home = () => {
       {articles.map((data) => {
         return (
           <>
-            <div className="border-2 p-6 rounded-tr-lg rounded-l-xl">
-              <p className="border-2 border-emerald-600 font-medium bg-emerald-500 rounded-full  text-center flex justify-center w-1/2">
+            <div className=" mt-5  mb-5 border-2 p-6 rounded-tr-lg rounded-l-xl">
+              <p className=" py-2 border-emerald-600 font-medium bg-emerald-500 rounded-full  text-center flex justify-center w-1/2 text-white">
                 {"#" + data.name}
               </p>
               <h1 className="text-3xl font-bold mt-5">{data.title}</h1>
-              <p className="mt-5">{data.content}</p>
+              <p className="mt-5 leading-1 tracking-wider">{data.content}</p>
+              <div className="row flex justify-between items-center  mt-5">
+                <button className="rounded-full bg-cyan-600 text-white px-5 py-1">
+                  Read More
+                </button>
+                <h3 className="font-semibold">By - {data.author}</h3>
+              </div>
             </div>
           </>
         );
