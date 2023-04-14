b027a84f3674e2f388dc899bfbc5cb4b46305b57
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-14 15:00:46 +0530

    started working on json data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import { articles } from "./ArticleContent";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Hi welcome to my blog!</h1>
-      <p className="mt-5 leading-5 tracking-widest font-Georgia">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-        dignissimos natus iure inventore provident, quam libero similique
-        repellendus voluptatibus voluptatum, saepe harum? Sequi deserunt dolores
-        culpa temporibus deleniti asperiores minima.
-      </p>
-      <button className=" bg-red-600 rounded mt-10 pt-2 pb-2 pl-3 pr-3 text-center flex justify-center font-bold  bg-transparent text-black border border-blue-800">
-        Click me
-      </button>
-    </div>
+    <>
+      <div>
+        {articles.map((data, key) => {
+          return (
+            <div key={key}>
+              <h1>{data}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
